e111a677ab47df52fb74fd561949710f55d36e57
Gorgi <gorgimitev@gmail.com>
2021-03-24 14:19:08 +0100

    adjusting rubocop file

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,21 +1,21 @@
 def bubble_sort_by(arr)
-    sorted = false
-    until sorted
+  sorted = false
+  until sorted
     sorted = true
     arr.each_with_index do |item, index|
-        next unless arr[index + 1] && yield(item, arr[index + 1]).positive?
-        
-        arr[index] = arr[index + 1]
-        arr[index + 1] = item
-        sorted = false
-        end
+      next unless arr[index + 1] && yield(item, arr[index + 1]).positive?
+
+      arr[index] = arr[index + 1]
+      arr[index + 1] = item
+      sorted = false
     end
-    
-    if sorted == true
+  end
+
+  if sorted == true
     arr
-    else
+  else
     bubble_sort_by(arr)
-    end
-end 
+  end
+end
 
 # print bubble_sort_by(["halloween","hi","hello","hey"]) { |left,right| left.length - right.length }
