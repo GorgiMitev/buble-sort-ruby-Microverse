d4251d661f3a2ffccd96ca9b2af80909222149c9
Gorgi <gorgimitev@gmail.com>
2021-03-24 14:09:51 +0100

    creating bubble sort assignments
    *creating and testing bubble_sort
    *creating and testing bubble_sort_rb

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(arr)
+    sorted = false
+    unless sorted 
+        sorted = true
+    arr.each_with_index do |item, index| 
+        next unless arr[index + 1] && item > arr[index + 1]
+        arr[index] = arr[index + 1]
+        arr[index + 1] = item
+        sorted = false
+        end
+    end
+    if sorted == true
+        return arr
+    else
+        bubble_sort(arr)    
+    end
+end
+
+# print bubble_sort([1, 3, 2, 7, 6])
